c9630fad8657c775d2fa8383588828b529e1dee8
Binglun Li (Alan) <47376247+libinglun@users.noreply.github.com>
2026-06-06 11:05:59 +0100

    Add: count-paths - submission-1

---
+++ Data Structures & Algorithms/count-paths/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        # 2D DP: dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        # time and space complexity O(m * n)
+        '''
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        dp[1][1] = 1
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if i == 1 and j == 1:
+                    continue
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        
+        return dp[m][n]
+        '''
+        # Space can be optimised dp[j] += dp[j - 1]
+        dp = [1] * n
+        for i in range(m - 1):
+            for j in range(1, n):
+                # 0 index directly copies the last result
+                dp[j] += dp[j - 1]
+
+        return dp[n-1]
+
